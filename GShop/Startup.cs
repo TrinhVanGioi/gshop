11c0f032071e7fdba472522b65f04f83ee6998d9
GT <GT@DESKTOP-PV98F63>
2019-07-12 13:36:33 +0700

    Add project files.

---
+++ GShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GShop.Startup))]
+namespace GShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
