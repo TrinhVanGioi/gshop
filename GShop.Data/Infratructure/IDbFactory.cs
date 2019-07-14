430ac09b2a3cae4f283f0d3a45ad9c49daa5675d
GT <GT@DESKTOP-PV98F63>
2019-07-14 15:09:17 +0700

    Update code

--- GShop.Data/Infratructure/IDbFactory.cs
+++ GShop.Data/Infratructure/IDbFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GShop.Data.Infratructure
 {
-    pu interface IDbFactory: IDisposable
+    public interface IDbFactory: IDisposable
     {
         GShopDbContext Init();
     }
